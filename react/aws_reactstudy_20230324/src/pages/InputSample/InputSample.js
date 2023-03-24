8d5b30a11966d7a71c31323a76c4678a87adee53
JungSungHyun <hhgg0017@gmail.com>
2023-03-24 12:18:47 +0900

    2023 03. 24. react

--- react/aws_reactstudy_20230324/src/pages/InputSample/InputSample.js
+++ react/aws_reactstudy_20230324/src/pages/InputSample/InputSample.js
@@ -1,21 +1,49 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const InputSample = () => {
-    const[username,setUsername] = useState("");
-    const[password, setPassword] = useState("");
+    const userInfo = {
+        username: '',
+        password: ''
+    }
+    const [userInput, setUserInput] = useState(userInfo);
+    const [userInputText, setUserInputText] = useState(userInfo);
+    
+    const { username, password } = userInputText;
+
+    const passwordRef = useRef();
 
     const handlerChange= (e) =>{
-        if(e.target.name === "username"){
-            setUsername(e.target.value);
-        }else{
-            setPassword(e.target.value);
+        const {name, value} = e.target;
+        setUserInput({...userInput, [name]: value}); // 기존의 값을 key: value로 덮음
+    }
+
+    const nextFocus = (e) =>{
+        if(e.keyCode === 13){
+            passwordRef.current.focus();
+        }
+    }
+
+    const submitHandler = (e) =>{
+        if(e.keyCode === 13){
+            setUserInputText({...userInput});
         }
     }
     
     return (
         <div>
-            <input type="text" onChange={handlerChange} name="username" />
-            <input type="text" onChange={handlerChange} name="password" />
+            <input 
+                type="text" 
+                onChange={handlerChange} 
+                onKeyUp={nextFocus} 
+                name="username"
+            />
+            <input 
+                type="text"
+                onChange={handlerChange}
+                onKeyUp={submitHandler} 
+                name="password" 
+                ref={passwordRef}
+            />
             <div>username: {username}</div>
             <div>password: {password}</div>
         </div>
