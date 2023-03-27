31cb2531316c6c550f24753bbe93e2083c24d8e1
JungSungHyun <hhgg0017@gmail.com>
2023-03-27 12:14:32 +0900

    2023. 03. 27 userList update, delete add

--- react/aws_reactstudy_20230324/src/pages/UserList/UserList.js
+++ react/aws_reactstudy_20230324/src/pages/UserList/UserList.js
@@ -4,24 +4,27 @@ import * as S from "./style";
 
 const UserList = () => {
   useEffect(() => {
-    console.log("component rendering");
+    console.log("컴포넌트 렌더링");
   }, []);
 
   const user = {
     id: 0,
     username: "",
-    pasword: "",
+    password: "",
     name: "",
     email: "",
+    modifyFlag: false, //각각의 유저마다 boolean값을 넣어서 수정할지 안할지 선택하기위해 Flag를 만듬
   };
 
-  const [inputs, setInput] = useState(user);
+  const userIndex = useRef(1);
+  const [users, setUsers] = useState([]);
+  const [inputs, setInputs] = useState(user);
   const inputRefs = [useRef(), useRef(), useRef(), useRef()];
   const addButtonRef = useRef();
 
   const inputHandler = (e) => {
     const { name, value } = e.target;
-    setInput({ ...inputs, [name]: value });
+    setInputs({ ...inputs, [name]: value });
   };
 
   const keyupHandler = (e) => {
@@ -46,33 +49,55 @@ const UserList = () => {
     }
   };
 
-  const addHandler = () => {};
+  const addHandler = () => {
+    const user = {
+      ...inputs,
+    };
 
-  const users = [
-    {
-      id: 1,
-      username: "aaa",
-      password: "1234",
-      name: "AAA",
-      email: "aaa@gmail.com",
-    },
-    {
-      id: 2,
-      username: "bbb",
-      password: "1234",
-      name: "bbb",
-      email: "bbb@gmail.com",
-    },
-    {
-      id: 3,
-      username: "ccc",
-      password: "1234",
-      name: "ccc",
-      email: "ccc@gmail.com",
-    },
-  ];
+    user.id = userIndex.current++;
 
-  const userIndex = useRef(4);
+    setUsers([...users, user]);
+  };
+
+  const onRemove = (index) => {
+    // users.splice(index - 1, 1);
+    // setUsers([...users]);
+
+    setUsers(users.filter((user) => user.id !== index));
+  };
+
+  const onModify = (index) => {
+    //해당 유저를 수정하는 함수
+    setUsers(
+      //setUsers를 이용해 유저의 상태가 변화된다.
+      users.map((user) => {
+        //map을 써서 해당 유저와 클릭했을때 해당 user.id의 index의 값이 같으면
+        if (user.id === index) {
+          setInputs({ ...user }); // 해당 유저의 수정된 값을 setInputs을 이용해 값을 전달하고
+          user.modifyFlag = true; // 수정을 했으므로 true
+        } else {
+          user.modifyFlag = false; // 아니면 false
+        }
+        return user;
+      })
+    );
+  };
+
+  const onSave = (index) => {
+    // 해당 유저의 수정된 값을 저장.
+    setUsers(
+      users.map((user) => {
+        if (user.id === index) {
+          user.modifyFlag = true;
+          return {
+            ...inputs,
+            id: user.id,
+          };
+        }
+        return user;
+      })
+    );
+  };
 
   return (
     <div css={S.container}>
@@ -83,7 +108,6 @@ const UserList = () => {
           onChange={inputHandler}
           placeholder="username"
           name="username"
-          value={0}
           ref={inputRefs[0]}
         />
         <input
@@ -92,7 +116,6 @@ const UserList = () => {
           onChange={inputHandler}
           placeholder="password"
           name="password"
-          value={0}
           ref={inputRefs[1]}
         />
         <input
@@ -101,7 +124,6 @@ const UserList = () => {
           onChange={inputHandler}
           placeholder="name"
           name="name"
-          value={0}
           ref={inputRefs[2]}
         />
         <input
@@ -110,7 +132,6 @@ const UserList = () => {
           onChange={inputHandler}
           placeholder="email"
           name="email"
-          value={0}
           ref={inputRefs[3]}
         />
         <button type="button" onClick={addHandler} ref={addButtonRef}>
@@ -125,18 +146,86 @@ const UserList = () => {
             <th css={S.ThAndTd}>password</th>
             <th css={S.ThAndTd}>name</th>
             <th css={S.ThAndTd}>email</th>
+            <th css={S.ThAndTd}>update</th>
+            <th css={S.ThAndTd}>delete</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => {
-            userIndex.current += 1;
             return (
-              <tr>
+              //key라는 props를 이용해서 이 부분만 재랜더링을 할 수 있다.
+              <tr key={user.id}>
                 <td css={S.ThAndTd}>{user.id}</td>
-                <td css={S.ThAndTd}>{user.username}</td>
-                <td css={S.ThAndTd}>{user.password}</td>
-                <td css={S.ThAndTd}>{user.name}</td>
-                <td css={S.ThAndTd}>{user.email}</td>
+                <td css={S.ThAndTd}>
+                  {user.modifyFlag ? ( //수정버튼을 누르면 input태그로 바뀐다. modifyFlag를 이용해서
+                    <input
+                      type="text"
+                      onKeyUp={keyupHandler}
+                      onChange={inputHandler}
+                      placeholder="username"
+                      name="username"
+                      ref={inputRefs[0]}
+                      defaultValue={user.username}
+                    />
+                  ) : (
+                    user.username
+                  )}
+                </td>
+                <td css={S.ThAndTd}>
+                  {user.modifyFlag ? (
+                    <input
+                      type="text"
+                      onKeyUp={keyupHandler}
+                      onChange={inputHandler}
+                      placeholder="password"
+                      name="password"
+                      ref={inputRefs[1]}
+                      defaultValue={user.password}
+                    />
+                  ) : (
+                    user.password
+                  )}
+                </td>
+                <td css={S.ThAndTd}>
+                  {user.modifyFlag ? (
+                    <input
+                      type="text"
+                      onKeyUp={keyupHandler}
+                      onChange={inputHandler}
+                      placeholder="name"
+                      name="name"
+                      ref={inputRefs[2]}
+                      defaultValue={user.name}
+                    />
+                  ) : (
+                    user.name
+                  )}
+                </td>
+                <td css={S.ThAndTd}>
+                  {user.modifyFlag ? (
+                    <input
+                      type="text"
+                      onKeyUp={keyupHandler}
+                      onChange={inputHandler}
+                      placeholder="email"
+                      name="email"
+                      ref={inputRefs[3]}
+                      defaultValue={user.email}
+                    />
+                  ) : (
+                    user.email
+                  )}
+                </td>
+                <td css={S.ThAndTd}>
+                  {user.modifyFlag ? (
+                    <button onClick={() => onSave(user.id)}>확인</button> //user정보를 수정하기위해 두개의버튼을 만들고 수정할 유저만 수정시키기위해 user.id 즉 index값을 넘긴다.
+                  ) : (
+                    <button onClick={() => onModify(user.id)}>수정</button>
+                  )}
+                </td>
+                <td css={S.ThAndTd}>
+                  <button onClick={() => onRemove(user.id)}>삭제</button>
+                </td>
               </tr>
             );
           })}
